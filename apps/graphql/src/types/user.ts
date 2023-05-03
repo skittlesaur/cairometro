3e219abfe991a534854982f0141c15995061f53a
Youssef Saad <youssef.elbasha.saad@gmail.com>
2023-05-03 15:13:15 +0300

    updated user type

--- apps/graphql/src/types/user.ts
+++ apps/graphql/src/types/user.ts
@@ -1,14 +1,24 @@
-import { objectType } from 'nexus'
-import { User } from 'nexus-prisma'
+import { enumType, objectType } from 'nexus'
+import { User, UserRole } from 'nexus-prisma'
+
+// import UserRoleEnum from './user-role'
+
 
 const UserType = objectType({
   name: User.$name,
   definition(t) {
     t.field(User.id)
+    t.field('userRole', { type: UserRoleEnum })
     t.field(User.email)
     t.field(User.name)
     t.field(User.createdAt)
   },
 })
+
+const UserRoleEnum = enumType({
+  name: 'UserRoleEnum',
+  members: UserRole.members,
+})
+
 
 export default UserType
