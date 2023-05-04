554fb7c1dee774dd59e3652839a3e9e31c4a1448
baraa <hey.baraa@gmail.com>
2023-05-04 21:27:23 +0300

    seo improvements

--- apps/home/pages/index.tsx
+++ apps/home/pages/index.tsx
@@ -3,9 +3,13 @@ import type { NextPage } from 'next'
 import Home from '@/components/home'
 import AppLayout from '@/layouts/app'
 
+import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { NextSeo } from 'next-seo'
 
 const HomePage: NextPage = () => {
+  const { t } = useTranslation('home')
+
   return (
     <AppLayout
       navigation={{
@@ -13,14 +17,19 @@ const HomePage: NextPage = () => {
         activePath: '/',
       }}
     >
+      <NextSeo
+        title={t('seo.title') as string}
+        description={t('seo.description') as string}
+        titleTemplate="%s"
+      />
       <Home />
     </AppLayout>
   )
 }
 
-export const getStaticProps = async ({ locale }: {locale: string}) => ({
+export const getStaticProps = async ({ locale }: { locale: string }) => ({
   props: {
-    ...(await serverSideTranslations(locale, ['common'])),
+    ...(await serverSideTranslations(locale, ['common', 'home'])),
   },
 })
 
