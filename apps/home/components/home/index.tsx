045bd347bbbb47b59bb8c1d32674d37633f254e5
Youssef Saad <youssef.elbasha.saad@gmail.com>
2023-05-05 21:38:27 +0300

    Added Accordion

--- apps/home/components/home/index.tsx
+++ apps/home/components/home/index.tsx
@@ -1,9 +1,11 @@
+import Accordion from './accordion-test'
+
 const Home = () => {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      *add home components here*
-    </div>
-  )
+	return (
+		<div className="h-screen flex items-center justify-center">
+			<Accordion />
+		</div>
+	)
 }
 
 export default Home
