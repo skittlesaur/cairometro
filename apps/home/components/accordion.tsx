80b1cd3373ed4a8f746bfad954c1b555010e65ac
Youssef Saad <youssef.elbasha.saad@gmail.com>
2023-05-07 08:47:28 +0300

    Added Arabic localization

--- apps/home/components/accordion.tsx
+++ apps/home/components/accordion.tsx
@@ -4,24 +4,8 @@ import ChevronDownIcon from '@/icons/chevron-down.svg'
 
 import * as AccordionPrimitive from '@radix-ui/react-accordion'
 import cn from 'classnames'
-import { AnimatePresence, motion } from 'framer-motion'
 
 export const Accordion = AccordionPrimitive.Root
-
-// export const Accordion = React.forwardRef<
-//   React.ElementRef<typeof AccordionPrimitive.Root>,
-//   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Root>
-// >(({ children, className, ...props }, forwardedRef) => (
-//   <AccordionPrimitive.Root
-//     ref={forwardedRef}
-//     collapsible
-//     type="single"
-//     className={cn('space-y-5 ml-4 mr-4 w-full', className)}
-//     {...props}
-//   >
-//     {children}
-//   </AccordionPrimitive.Root>
-// ))
 
 Accordion.displayName = AccordionPrimitive.Root.displayName
 
@@ -50,7 +34,7 @@ export const AccordionTrigger = React.forwardRef<
   <AccordionPrimitive.Header className="flex">
     <AccordionPrimitive.Trigger
       className={cn(
-        'group flex flex-1 cursor-default items-center justify-between bg-white py-6 px-10 leading-none outline-none data-[state=open]:bg-[#FAFAFA] data-[state=open]:pb-0 text-[#171717] font-semibold text-xl rounded',
+        'group flex flex-1 cursor-default items-center justify-between bg-white py-6 px-10 leading-none outline-none data-[state=open]:bg-[#FAFAFA] data-[state=open]:pb-0 font-semibold text-xl rounded text-[#525252] hover:text-[#171717] data-[state=open]:text-[#171717] transition-colors',
         className
       )}
       {...props}
@@ -59,44 +43,26 @@ export const AccordionTrigger = React.forwardRef<
       {children}
       <ChevronDownIcon
         aria-hidden
-        className="h-5 w-5 text-[#A3A3A3] group-hover:text-[#404040] ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300 group-data-[state=open]:rotate-180 group-data-[state=open]:text-[#404040]"
+        className="h-5 w-5 text-[#A3A3A3] group-hover:text-[#404040] ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-all duration-300 group-data-[state=open]:rotate-180 group-data-[state=open]:text-[#404040] "
       />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
 AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName
 
-// interface AccordionContentProps
-//   extends React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content> {
-//   open?: boolean
-// }
-
 export const AccordionContent = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
 >(({ children, className, ...props }, forwardedRef) => (
-  <AnimatePresence>
-    <AccordionPrimitive.Content
-      asChild
-      className={cn('overflow-hidden bg-[#FAFAFA] rounded px-10 ', className)}
-      {...props}
-      ref={forwardedRef}
-    >
-      <motion.div
-        initial={{ height: 0 }}
-        animate={{ height: 'auto' }}
-        exit={{ height: 0 }}
-        transition={{
-          duration: 0.3,
-        }}
-      >
-        <div className="text-base text-[#525252] font-normal py-6">
-          {children}
-        </div>
-      </motion.div>
-    </AccordionPrimitive.Content>
-  </AnimatePresence>
+  <AccordionPrimitive.Content
+    className={cn(
+      'overflow-hidden bg-[#FAFAFA] rounded px-10 data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp',
+      className
+    )}
+    {...props}
+    ref={forwardedRef}
+  >
+    <div className="text-base text-[#525252] font-normal py-6">{children}</div>
+  </AccordionPrimitive.Content>
 ))
 AccordionContent.displayName = AccordionPrimitive.Content.displayName
-
-// todo add animation for accordion content to smoothly close when unfocused
--- apps/home/components/home/faq.tsx
+++ apps/home/components/home/faq.tsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import Link from 'next/link'
+
 import {
   Accordion,
   AccordionContent,
@@ -5,11 +8,15 @@ import {
   AccordionTrigger,
 } from '@/components/accordion'
 
+import { useTranslation } from 'next-i18next'
+
 const Faq = () => {
+  const { t } = useTranslation('home')
+
   return (
     <div className="w-full flex flex-col items-center">
       <h1 className="font-semibold text-5xl text-[#000000] mb-16">
-        Frequently Asked Questions
+        {t('faq.title')}
       </h1>
       <Accordion
         collapsible
@@ -18,25 +25,46 @@ const Faq = () => {
         className={'space-y-5 w-full'}
       >
         <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionTrigger>{t('faq.question1.title')}</AccordionTrigger>
           <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
+            {t('faq.question1.answer')
+              .split(' ')
+              .map((word, index) => (
+                <React.Fragment key={index}>
+                  {index !== 0 && ' '}
+                  {word === '{0}' ? (
+                    <Link
+                      className="text-link-text font-semibold"
+                      href={'/subscription'}
+                    >
+                      {t('faq.question1.subscription')}
+                    </Link>
+                  ) : (
+                    word
+                  )}
+                </React.Fragment>
+              ))}
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="item-2">
-          <AccordionTrigger>Is it unstyled?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It{"'"}s unstyled by default, giving you freedom over the look
-            and feel.
-          </AccordionContent>
+          <AccordionTrigger>{t('faq.question2.title')}</AccordionTrigger>
+          <AccordionContent>{t('faq.question2.answer')}</AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="item-3">
-          <AccordionTrigger>Can it be animated?</AccordionTrigger>
-          <AccordionContent>
-            Yes! You can animate the Accordion with CSS or JavaScript.
-          </AccordionContent>
+          <AccordionTrigger>{t('faq.question3.title')}</AccordionTrigger>
+          <AccordionContent>{t('faq.question3.answer')}</AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="item-4">
+          <AccordionTrigger>{t('faq.question4.title')}</AccordionTrigger>
+          <AccordionContent>{t('faq.question4.answer')}</AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="item-5">
+          <AccordionTrigger>{t('faq.question5.title')}</AccordionTrigger>
+          <AccordionContent>{t('faq.question5.answer')}</AccordionContent>
         </AccordionItem>
       </Accordion>
     </div>
@@ -44,3 +72,15 @@ const Faq = () => {
 }
 
 export default Faq
+
+// Create a new account by registering with your personal information
+//             and a valid email address or phone number. Navigate to the
+//             <Link
+//               className="text-link-text font-semibold"
+//               href={'/subscription'}
+//             >
+//               {' '}
+//               Subscription
+//             </Link>{' '}
+//             section of the platform. Select the subscription that meets your
+//             needs and process with the payment details.
