b07f184f558a7fa92d3c62a5e8e03a7257b607e4
baraa <hey.baraa@gmail.com>
2023-05-08 15:45:46 +0300

    removed unnecessary tag

--- apps/home/components/home/index.tsx
+++ apps/home/components/home/index.tsx
@@ -3,7 +3,7 @@ import Companion from './companion'
 const Home = () => {
   return (
     <div className="h-screen flex items-center justify-center">
-      <Companion></Companion>
+      <Companion />
     </div>
   )
 }
