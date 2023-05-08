5e43c897ec05f10fd4a1bb77aa57200accde7498
thethirdsh <momenyasser522@gmail.com>
2023-05-08 09:15:22 +0300

    Refactored the code a little bit

--- apps/home/components/home/companion.tsx
+++ apps/home/components/home/companion.tsx
@@ -1,114 +1,50 @@
+import HeartOutline from '@/icons/heart-outline-gradient.svg'
+import TicketOutline from '@/icons/ticket-outline-gradient.svg'
+import TimerOutline from '@/icons/timer-outline-gradient.svg'
+
 const Companion = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-96">
-      <div className="">
-        <strong>
-          <h1 className="text-5xl relative text-center">
-            Your Cairo Metro Companion
-          </h1>
-        </strong>
-        <br></br>
-        <p className="text-xl text-[#A3A3A3] align-[80px] leading-3">
+    <div className="flex flex-col items-center justify-center gap-16 h-96">
+      <div className="flex flex-col gap-3">
+        <h1 className="text-5xl relative text-center font-semibold">
+          Your Cairo Metro Companion
+        </h1>
+        <p className="text-xl text-neutral-400">
           Enjoy a stress-free commute with convenient metro schedules, payment
           options, alerts, and assistance.
         </p>
       </div>
-      <div className="container m-auto grid grid-cols-3 gap-8 w-auto">
-        <div className="tile">
-          <button className="justify-center items-center bg-[#FAFAFA] h-48 w-96 rounded-[12px] border">
-            <svg
-              className="w-11 h-15 text-[#DA1E79] relative inset-x-40"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-              />
-            </svg>
-            <br></br>
-            <strong>
-              <p className="text-lg leading-6">Live Support</p>
-            </strong>
-            <p className="text-sm leading-9">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div className="px-9 py-7 bg-neutral-50 border border-neutral-200 rounded-lg flex flex-col items-center justify-center gap-2">
+          <HeartOutline className="w-12 h-12" />
+          <div className="flex flex-col gap-1 items-center">
+            <p className="text-lg font-semibold">Live Support</p>
+            <p className="text-sm text-center">
               Get immediate assistance with the live support
             </p>
-          </button>
+          </div>
         </div>
-        <div className="tile">
-          <button className="justify-center items-center bg-[#FAFAFA] h-48 w-96 rounded-[12px] border">
-            <svg
-              className="h-10 w-14 relative inset-x-40"
-              fill="#327472"
-              version="1.1"
-              id="Layer_1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="796 796 200 200"
-              enable-background="new 796 796 200 200"
-            >
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                {" "}
-                <g>
-                  {" "}
-                  <path d="M995.998,871.884c0-3.136-1.222-6.084-3.439-8.301l-14.355-14.355c-1.479-1.479-3.445-2.294-5.538-2.294 c-1.906,0-3.747,0.694-5.181,1.958c-3.145,2.771-7.195,4.298-11.403,4.298c-4.614,0-8.953-1.796-12.216-5.059 c-6.43-6.43-6.764-16.805-0.762-23.617c2.729-3.094,2.582-7.804-0.335-10.721l-14.355-14.355c-2.219-2.216-5.166-3.438-8.301-3.438 c-3.136,0-6.083,1.221-8.302,3.438L799.434,911.813c-4.575,4.577-4.575,12.023,0,16.602l14.356,14.356 c1.479,1.479,3.446,2.296,5.539,2.296c1.908,0,3.748-0.695,5.182-1.959c3.146-2.772,7.195-4.299,11.404-4.299 c4.614,0,8.951,1.797,12.214,5.061c6.431,6.431,6.765,16.804,0.761,23.616c-2.73,3.096-2.581,7.807,0.336,10.719l14.354,14.355 c2.218,2.219,5.166,3.439,8.302,3.439s6.084-1.221,8.301-3.439l82.754-82.753c0.001-0.001,0.001-0.001,0.001-0.001s0,0,0.002-0.002 l29.618-29.618C994.776,877.969,995.998,875.021,995.998,871.884z M958.53,896.588l-1.469-1.469c-2.435-2.435-6.379-2.434-8.813,0 c-2.433,2.434-2.433,6.379,0.001,8.814l1.469,1.467l-77.835,77.836l-10.997-10.997c7.497-11.569,6.08-27.161-3.943-37.183 c-5.616-5.618-13.084-8.711-21.027-8.711c-5.772,0-11.375,1.672-16.157,4.769l-11-11l77.837-77.836l1.47,1.469 c1.217,1.217,2.811,1.825,4.406,1.825s3.19-0.609,4.407-1.826c2.434-2.434,2.434-6.379-0.001-8.813l-1.468-1.469l24.703-24.703 l10.997,10.997c-7.495,11.572-6.079,27.163,3.943,37.184c5.616,5.617,13.084,8.709,21.028,8.709c5.771,0,11.373-1.671,16.155-4.767 l10.998,10.998L958.53,896.588z"></path>{" "}
-                  <path d="M909.645,847.701c-2.432-2.434-6.381-2.434-8.813,0c-2.434,2.434-2.434,6.379,0,8.813l10.942,10.943 c1.216,1.216,2.812,1.825,4.406,1.825s3.19-0.608,4.406-1.825c2.434-2.434,2.434-6.379,0-8.813L909.645,847.701z"></path>{" "}
-                  <path d="M933.353,871.41c-2.431-2.433-6.38-2.433-8.812,0c-2.435,2.434-2.435,6.379,0,8.813l10.941,10.942 c1.217,1.217,2.813,1.826,4.406,1.826c1.595,0,3.19-0.608,4.406-1.825c2.435-2.434,2.435-6.379,0-8.813L933.353,871.41z"></path>{" "}
-                </g>{" "}
-              </g>
-            </svg>
-            <br></br>
-            <strong>
-              <p className="text-lg leading-6">Easy Tracking</p>
-            </strong>
-            <p className="text-sm leading-9">
+        <div className="px-9 py-7 bg-neutral-50 border border-neutral-200 rounded-lg flex flex-col items-center justify-center gap-2">
+          <TicketOutline className="w-12 h-12" />
+          <div className="flex flex-col gap-1 items-center">
+            <p className="text-lg font-semibold">Easy Tracking</p>
+            <p className="text-sm text-center">
               Purchase and store your metro tickets
             </p>
-          </button>
+          </div>
         </div>
-        <div className="tile">
-          <button className="justify-center items-center bg-[#FAFAFA] h-48 w-96 rounded-[12px] border">
-            <svg
-              className="h-9 w-14 relative inset-x-40 inset-y-3"
-              viewBox="0 0 16 16"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="#543DFA"
-            >
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                <path
-                  d="M572.329319,159.423805 L572.328547,159.424577 L568.225353,153.482367 C567.903341,153.016033 568.023632,152.908729 568.482182,153.225538 L574.341491,157.273698 C574.435816,157.332998 574.525044,157.403724 574.607193,157.485872 C575.195753,158.074433 575.198003,159.02643 574.612216,159.612216 C574.02643,160.198003 573.074433,160.195753 572.485872,159.607193 C572.428023,159.549343 572.375838,159.487983 572.329319,159.423805 Z M567,159 C567,162.865993 570.134007,166 574,166 C577.865993,166 581,162.865993 581,159 C581,155.473528 578.392294,152.556119 575,152.070889 L575,153.496087 C575,153.774391 574.768066,154 574.5,154 C574.223858,154 574,153.784247 574,153.504611 L574,152.752306 L574,151 C578.418278,151 582,154.581722 582,159 C582,163.418278 578.418278,167 574,167 C569.581722,167 566,163.418278 566,159 L567,159 Z"
-                  transform="translate(-566 -151)"
-                ></path>{" "}
-              </g>
-            </svg>
-            <br></br>
-            <strong>
-              <p className="text-lg leading-10">Realtime Schedule</p>
-            </strong>
-            <p className="text-sm leading-4">
-              Plan your journey with confidence with realtime metro arrivals and
-              departures
+        <div className="px-9 py-7 bg-neutral-50 border border-neutral-200 rounded-lg flex flex-col items-center justify-center gap-2">
+          <TimerOutline className="w-12 h-12" />
+          <div className="flex flex-col gap-1 items-center">
+            <p className="text-lg font-semibold">Realtime Schedule</p>
+            <p className="text-sm text-center">
+              Plan your journey with confidence with realtime metro arrivals and departures
             </p>
-          </button>
+          </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Companion;
+export default Companion
