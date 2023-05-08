3c1afc7baad1a0054529bd20f699a5dffe8bdd45
baraa <hey.baraa@gmail.com>
2023-05-08 15:58:56 +0300

    add translations

--- apps/home/components/home/companion.tsx
+++ apps/home/components/home/companion.tsx
@@ -2,43 +2,52 @@ import HeartOutline from '@/icons/heart-outline-gradient.svg'
 import TicketOutline from '@/icons/ticket-outline-gradient.svg'
 import TimerOutline from '@/icons/timer-outline-gradient.svg'
 
+import { useTranslation } from 'next-i18next'
+
 const Companion = () => {
+  const { t } = useTranslation('home')
+
   return (
-    <div className="flex flex-col items-center justify-center gap-16 h-96">
+    <div className="flex flex-col items-center justify-center gap-16">
       <div className="flex flex-col gap-3">
-        <h1 className="text-5xl relative text-center font-semibold">
-          Your Cairo Metro Companion
+        <h1 className="text-5xl relative ltr:text-left rtl:text-right md:!text-center font-semibold">
+          {t('companion.title')}
         </h1>
-        <p className="text-xl text-neutral-400">
-          Enjoy a stress-free commute with convenient metro schedules, payment
-          options, alerts, and assistance.
+        <p className="text-xl text-neutral-400 ltr:text-left rtl:text-right md:!text-center">
+          {t('companion.subtitle')}
         </p>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-        <div className="px-9 py-7 bg-neutral-50 border border-neutral-200 rounded-lg flex flex-col items-center justify-center gap-2">
+        <div className="px-9 py-7 bg-neutral-50 border border-neutral-200 rounded-lg flex flex-col items-center justify-start gap-2">
           <HeartOutline className="w-12 h-12" />
           <div className="flex flex-col gap-1 items-center">
-            <p className="text-lg font-semibold">Live Support</p>
+            <p className="text-lg font-semibold">
+              {t('companion.liveSupport.title')}
+            </p>
             <p className="text-sm text-center">
-              Get immediate assistance with the live support
+              {t('companion.liveSupport.description')}
             </p>
           </div>
         </div>
-        <div className="px-9 py-7 bg-neutral-50 border border-neutral-200 rounded-lg flex flex-col items-center justify-center gap-2">
+        <div className="px-9 py-7 bg-neutral-50 border border-neutral-200 rounded-lg flex flex-col items-center justify-start gap-2">
           <TicketOutline className="w-12 h-12" />
           <div className="flex flex-col gap-1 items-center">
-            <p className="text-lg font-semibold">Easy Tracking</p>
+            <p className="text-lg font-semibold">
+              {t('companion.easyTracking.title')}
+            </p>
             <p className="text-sm text-center">
-              Purchase and store your metro tickets
+              {t('companion.easyTracking.description')}
             </p>
           </div>
         </div>
-        <div className="px-9 py-7 bg-neutral-50 border border-neutral-200 rounded-lg flex flex-col items-center justify-center gap-2">
+        <div className="px-9 py-7 bg-neutral-50 border border-neutral-200 rounded-lg flex flex-col items-center justify-start gap-2">
           <TimerOutline className="w-12 h-12" />
           <div className="flex flex-col gap-1 items-center">
-            <p className="text-lg font-semibold">Realtime Schedule</p>
+            <p className="text-lg font-semibold">
+              {t('companion.realtimeSchedule.title')}
+            </p>
             <p className="text-sm text-center">
-              Plan your journey with confidence with realtime metro arrivals and departures
+              {t('companion.realtimeSchedule.description')}
             </p>
           </div>
         </div>
