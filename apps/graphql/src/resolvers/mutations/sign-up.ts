cb8caea1238a06bc877ae8c6dd396292e97bf91e
Youssef Saad <youssef.elbasha.saad@gmail.com>
2023-05-09 21:21:27 +0300

    added verification to document url

--- apps/graphql/src/resolvers/mutations/sign-up.ts
+++ apps/graphql/src/resolvers/mutations/sign-up.ts
@@ -25,15 +25,20 @@ const signUp: FieldResolver<'Mutation', 'signup'> = async (_, args, ctx: Context
     throw new GraphQLError('We Caught You ;) GG!!!')
 
   let user
+  const urlRegexPattern = /^https:\/\/res\.cloudinary\.com\/.*\/cairo-metro\/.*\.(png|jpe?g|gif|bmp|svg)$/igm
 
   if (args.userRole.userRole === 'SENIOR') {
+
+    if (!urlRegexPattern.test(args.documentUrl))
+      throw new GraphQLError('ID photo cannot be found, please retry uploading it')
+
     user = await prisma.user.create({
       data: {
         role: args.userRole.userRole,
         email: args.email,
         name: args.name,
         documentUrl: args.documentUrl,
-        verified: false,
+        documentVerified: false,
       },
     })
   } else {
