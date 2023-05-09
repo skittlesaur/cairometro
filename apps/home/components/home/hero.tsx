dece4cb8469146b6ee102f4e176d719bb11cc715
baraa <hey.baraa@gmail.com>
2023-05-09 16:06:14 +0300

    hero improvements

--- apps/home/components/home/hero.tsx
+++ apps/home/components/home/hero.tsx
@@ -3,11 +3,13 @@ import { useMemo } from 'react'
 import { Button } from '@/components/button'
 
 import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api'
+import { useTranslation } from 'next-i18next'
 import * as process from 'process'
 
 import { Separator } from '../separator'
 
 const Hero = () => {
+  const { t } = useTranslation('home')
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? '',
   })
@@ -16,7 +18,7 @@ const Hero = () => {
 
 
   return (
-    <div className="w-screen mx-[calc((100vw-100%)/-2+8px)]">
+    <div className="w-screen -translate-x-4 md:translate-x-0 md:mx-[calc((100vw-100%)/-2+8px)]">
       <div className="relative">
         {isLoaded ? (
           <GoogleMap
@@ -29,66 +31,66 @@ const Hero = () => {
             <Marker position={center} />
           </GoogleMap>
         ) : (
-          <div className="w-full h-[80vh] bg-neutral-300 animate-pulse" />
+          <div className="w-full h-[80vh] bg-neutral-200 animate-pulse" />
         )}
         <div className="absolute inset-0 bg-gradient-to-t from-white via-white/40 to-transparent pointer-events-none" />
       </div>
-      <div className="-translate-y-1/2 flex md:flex-row justify-between flex-col items-center border border-neutral-300 rounded-lg shadow-xl bg-white px-9 py-7 mx-auto max-w-[1300px]">
-        <div>
+      <div className="-translate-y-1/2 gap-4 md:gap-8 flex md:flex-row justify-between flex-col items-center border border-neutral-300 rounded-lg shadow-xl bg-white px-5 py-4 md:px-9 md:py-7 mx-4 md:mx-auto md:w-full max-w-[1300px]">
+        <div className="flex flex-col items-start w-full">
           <label className="text-base font-medium text-base-black block">
-            From
+            {t('hero.from.title')}
           </label>
           <input
-            placeholder="Departure"
+            placeholder={t('hero.from.placeholder') as string}
             className="font-normal text-sm leading-5 text-neutral-500"
           />
         </div>
         <Separator
           vertical
-          className="w-[1px] h-10 bg-neutral-200"
+          className="hidden md:block w-[1px] h-10 bg-neutral-200"
         />
-        <div>
+        <div className="flex flex-col items-start w-full">
           <label className="text-base font-medium text-base-black block">
-            To
+            {t('hero.to.title')}
           </label>
           <input
-            placeholder="Destination"
+            placeholder={t('hero.to.placeholder') as string}
             className="font-normal text-sm leading-5 text-neutral-500"
           />
         </div>
         <Separator
           vertical
-          className="w-[1px] h-10 bg-neutral-200"
+          className="hidden md:block w-[1px] h-10 bg-neutral-200"
         />
-        <div>
+        <div className="flex flex-col items-start w-full">
           <label className="text-base font-medium text-base-black block">
-            Travel Time
+            {t('hero.travelTime.title')}
           </label>
           <input
-            placeholder="20 March at 14:06"
+            placeholder={t('hero.travelTime.placeholder') as string}
             className="font-normal text-sm leading-5 text-neutral-500"
           />
         </div>
         <Separator
           vertical
-          className="w-[1px] h-10 bg-neutral-200"
+          className="hidden md:block w-[1px] h-10 bg-neutral-200"
         />
-        <div>
+        <div className="flex flex-col items-start w-full">
           <label className="text-base font-medium text-base-black block">
-            Passengers
+            {t('hero.passengers.title')}
           </label>
           <input
-            placeholder="1 Adult"
+            placeholder={t('hero.passengers.placeholder') as string}
             className="font-normal text-sm leading-5 text-neutral-500"
           />
         </div>
-        <div>
+        <div className="flex flex-col items-start w-full">
           <Button
             size={'lg'}
             variant={'primary'}
-            className="min-w-[14em]"
+            className="min-w-[14em] w-full"
           >
-            Find a Ride
+            {t('hero.findARide')}
           </Button>
         </div>
       </div>
