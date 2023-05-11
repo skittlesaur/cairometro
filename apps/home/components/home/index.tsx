8bd36d389a6f8e47bbfdc935af7d0b0bee5b28d2
Omar_ElBarbary <omar.m.elbarbary@gmail.com>
2023-05-12 00:39:07 +0300

    import fix

--- apps/home/components/home/index.tsx
+++ apps/home/components/home/index.tsx
@@ -2,6 +2,8 @@ import Companion from '@/components/home/companion'
 import Faq from '@/components/home/faq'
 import Hero from '@/components/home/hero'
 
+import Discover from './discover'
+
 const Home = () => {
   return (
     <div className="flex flex-col gap-16 md:gap-40 mb-16 md:mb-40">
