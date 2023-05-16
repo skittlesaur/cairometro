a8e2e9c0842d17c235877b40cc08f9dee4eb8588
Youssef Saad <youssef.elbasha.saad@gmail.com>
2023-05-17 00:02:36 +0300

    added data picker component

--- apps/home/components/calendar/calendar-components.tsx
+++ apps/home/components/calendar/calendar-components.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 
 import cn from 'classnames'
-import { DayPicker } from 'react-day-picker'
+import { DateFormatter, DayPicker } from 'react-day-picker'
 
 import ChevronLeft from '../../icons/chevron-left.svg'
 import ChevronRight from '../../icons/chevron-right.svg'
@@ -16,38 +16,61 @@ const Calendar = ({
   showOutsideDays = true,
   ...props
 }: CalendarProps) => {
+
+
+  const formatWeekdayName: DateFormatter = (weekday) => {
+    switch (weekday.getDay()){
+    case 0:
+      return 'SUN'
+    case 1:
+      return 'MON'
+    case 2:
+      return 'TUE'
+    case 3:
+      return 'WED'
+    case 4:
+      return 'THU'
+    case 5:
+      return 'FRI'
+    case 6:
+      return 'SAT'
+    }
+  }
+
+
   return (
     <DayPicker
+      formatters={{ formatWeekdayName }}
+      // fixedWeeks
       showOutsideDays={showOutsideDays}
-      className={cn('p-3', className)}
+      className={cn('', className)}
       classNames={{
         months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
-        month: 'space-y-4',
-        caption: 'flex justify-center pt-1 relative items-center',
-        caption_label: 'text-sm font-medium',
+        month: 'space-y-2',
+        caption: 'flex flow-row  relative items-center',
+        caption_label: 'pl-1 text-sm font-semibold',
         nav: 'space-x-1 flex items-center',
-        nav_button_previous: 'absolute right-8 bg-neutral-500 rounded-full',
-        nav_button_next: 'absolute right-1 bg-neutral-500 rounded-full',
+        nav_button_previous: 'absolute right-6 rounded-full hover:bg-neutral-100',
+        nav_button_next: 'absolute right-1 rounded-full hover:bg-neutral-100',
         table: 'w-full border-collapse space-y-1',
-        head_row: 'flex', 01090611704
-        head_cell:
-          'text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]',
-        row: 'flex w-full mt-2',
-        cell: 'text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
-        day: 'h-9 w-9 p-0 font-normal aria-selected:opacity-100',
+        head_row: 'flex gap-1',
+        head_cell: 'text-neutral-400/60 w-9 font-normal text-xs',
+        row: 'flex w-full mt-1 gap-1',
+        cell: 'text-center text-large p-0 relative focus-within:relative focus-within:z-20',
+        day: 'h-9 w-9 p-0 aria-selected:opacity-100 rounded-full hover:bg-neutral-100 hover:font-medium',
         day_selected:
-          'bg-red-100 text-primary hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
-        day_today: 'bg-accent text-accent-foreground',
-        day_outside: 'text-muted-foreground opacity-50',
-        day_disabled: 'text-muted-foreground opacity-50',
+          'text-primary font-medium bg-primary/10 rounded-full hover:bg-primary/10 hover:font-medium',
+        day_today: 'text-primary',
+        day_outside: 'invisible',
+        day_disabled: 'text-neutral-400 hover:bg-white hover:font-normal',
         day_range_middle:
-          'aria-selected:bg-accent aria-selected:text-accent-foreground',
+          '',
         day_hidden: 'invisible',
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4 text-black" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: ({ ...props }) => <ChevronLeft className={cn('h-4 w-4 text-primary')} />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4 text-primary" />,
       }}
       {...props}
     />
--- apps/home/components/calendar/calendar.tsx
+++ apps/home/components/calendar/calendar.tsx
@@ -1,41 +1,58 @@
-import React, { useState } from 'react'
+import React, { forwardRef, useImperativeHandle, useState } from 'react'
 
 import cn from 'classnames'
 import { format } from 'date-fns'
 
 import CalendarIcon from '../../icons/calendar.svg'
 
-import { Button } from './calendar-buttons'
 import { Calendar } from './calendar-components'
 import { Popover, PopoverContent, PopoverTrigger } from './calendar-popover'
+import TimePicker from './time-picker'
 
-const DatePicker = () => {
+const DatePicker = forwardRef((_, ref) => {
   const [date, setDate] = useState<Date>()
+  
+  const yesterday = new Date()
+  yesterday.setDate(new Date().getDate() - 1)
+  const disabledDays = { from: new Date(2023, 4, 1), to: yesterday }
+
+  useImperativeHandle(ref, () => ({
+    getResult: () => ({
+      date,
+    }),
+  }))
 
   return (
     <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={'outline'}
+      <PopoverTrigger
+        asChild
+        className="hover:bg-black"
+      >
+        <button
           className={cn(
-            'w-[280px] justify-start text-left font-normal',
+            'w-[280px] justify-start text-left font-normal', 'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background', 'border border-input hover:bg-black hover:text-white', 'h-10 py-2 px-4',
             !date && 'text-black'
           )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, 'PPP') : <span>Pick a date</span>}
-        </Button>
+          {date ? format(date, 'd MMMM') + ' at time' : <span>Pick a date</span>}
+        </button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
         <Calendar
           initialFocus
           mode="single"
+          fromMonth={new Date()}
+          disabled={disabledDays}
           selected={date}
           onSelect={setDate}
         />
+        <TimePicker />
       </PopoverContent>
     </Popover>
   )
-}
+})
+
+DatePicker.displayName = 'DatePicker'
 
 export default DatePicker
