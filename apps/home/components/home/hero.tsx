90a89d89e1b024aa5ae69c8714b272d9c87cb44f
baraa <hey.baraa@gmail.com>
2023-05-20 15:27:30 +0300

    test transition

--- apps/home/components/home/hero.tsx
+++ apps/home/components/home/hero.tsx
@@ -11,8 +11,31 @@ const Hero = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? '',
   })
-  const [selection, setSelection] = useState<{from?: Station, to?: Station}>()
-  const center = useMemo(() => ({ lat: 30.0444, lng: 31.2357 }), [])
+  const [selection, setSelection] = useState<{ from?: Station, to?: Station }>()
+  const center = useMemo(() => {
+    const from = selection?.from?.locationLngLat
+    const to = selection?.to?.locationLngLat
+    if (!from && !to) 
+      return ({ lat: 30.0444, lng: 31.2357 })
+    
+    if (from && !to) {
+      return from
+    }
+    
+    if (!from && to) {
+      return to
+    }
+    
+    if (!from || !to) return ({ lat: 30.0444, lng: 31.2357 })
+    
+    const lat = (from.lat + to.lat) / 2
+    const lng = (from.lng + to.lng) / 2
+
+    return {
+      lat,
+      lng,
+    }
+  }, [selection?.from?.locationLngLat, selection?.to?.locationLngLat])
 
   return (
     <div className="w-screen -mx-4 md:mx-[calc((100vw-100%)/-2+8px)]">
@@ -44,7 +67,12 @@ const Hero = () => {
         <div className="absolute inset-0 bg-gradient-to-t from-white via-white/40 to-transparent pointer-events-none" />
       </div>
       <TicketSearch
-        dynamicLocationUpdate={({ from, to }: {from?: Station, to?: Station}) => setSelection(prev => ({ ...prev, from, to }))}
+        dynamicLocationUpdate={({ from, to }: { from?: Station, to?: Station }) => {
+          if (from)
+            setSelection(prev => ({ ...prev, from }))
+          if (to)
+            setSelection(prev => ({ ...prev, to }))
+        }}
       />
     </div>
   )
