c80962d50dbe8868d4999353b3f45fe264e6d28d
baraa <hey.baraa@gmail.com>
2023-05-22 23:55:44 +0300

    fixes

--- apps/home/components/modal/purchase/visa-card.tsx
+++ apps/home/components/modal/purchase/visa-card.tsx
@@ -15,7 +15,7 @@ const VisaCard = ({ validThru, cardHolder, formattedCardNumber }: DefaultCardPro
       animate={{ opacity: 1, scale: 1, transition: { delay: 0.1 } }}
       exit={{ opacity: 0, scale: 0.8 }}
       transition={{ duration: 0.1 }}
-      className="relative z-[1] bg-gradient-to-br from-[#52B6FE] to-[#6154FE] text-white p-8 flex flex-col justify-end w-full md: min-h-40 aspect-video rounded-2xl font-mono"
+      className="relative z-[1] bg-gradient-to-br from-[#52B6FE] to-[#6154FE] text-white p-8 flex flex-col justify-end  w-full md:w-2/5 min-h-40 aspect-video rounded-2xl font-mono"
     >
       <p className="text-sm">
         {validThru || 'MM/YY'}
