e26ba6088bbdb5953da05af6b38f527181e2148a
Youssef Saad <youssef.elbasha.saad@gmail.com>
2023-06-08 00:12:30 +0300

    finished animation

--- apps/home/components/authentication/login/login-screen-animation.tsx
+++ apps/home/components/authentication/login/login-screen-animation.tsx
@@ -1,9 +1,14 @@
 import Radial from '@/components/radial-animation'
 
+import PerspectiveGrid from './perspective-grid'
+
 const LoginScreenAnimation = () => {
+
   return (
-    <div className="h-screen flex flex-col grow gap-10 items-center justify-center ltr:border-l rtl:border-r relative">
-      <div className="absolute bottom-[10%]">
+    <div className="h-screen flex flex-col grow gap-10 items-center justify-center ltr:border-l rtl:border-r relative overflow-hidden">
+      <PerspectiveGrid />
+
+      <div className="absolute bottom-[10%] z-0">
         <Radial character="M" />
       </div>
     </div>
