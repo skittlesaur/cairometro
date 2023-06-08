4bb0db1c92cc8c1e240b59ca4bf0ca2d280785e9
sbayce <youssef2311@gmail.com>
2023-06-08 21:13:45 +0300

    .

--- apps/home/components/continue-with-google.tsx
+++ apps/home/components/continue-with-google.tsx
@@ -2,19 +2,46 @@ import { Button } from '@/components/button'
 import LogoGoogle from '@/icons/logo-google.svg'
 
 import { useTranslation } from 'next-i18next'
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    gapi: any;
+  }
+}
 
 const ContinueWithGoogle = () => {
   const { t } = useTranslation('common')
+
+  useEffect(() => {
+    const loadGoogleSignInScript = () => {
+      const script = document.createElement('script');
+      script.src = 'https://apis.google.com/js/platform.js';
+      script.onload = initializeGoogleSignIn;
+      document.body.appendChild(script);
+    };
+
+    const initializeGoogleSignIn = () => {
+      window.gapi.load('auth2', () => {
+        window.gapi.auth2.init({
+          client_id: '930736154470-u2d8i2m2porafajd1lldajq0ih7jor39.apps.googleusercontent.com',
+        });
+      });
+    };
+
+    loadGoogleSignInScript();
+  }, []);
+
+  const handleSignIn = () => {
+    window.gapi.auth2.getAuthInstance().signIn();
+  };
 
   return (
     <Button
       className="w-full flex items-center justify-center gap-2 text-neutral-500 hover:text-neutral-600"
       variant="outline"
       size="lg"
-      onClick={() => {
-        // @TODO: Implement Google Sign In
-        alert('Google Sign In')
-      }}
+      onClick={handleSignIn}
     >
       <LogoGoogle className="w-6 h-6" />
       <span className="text-sm font-medium">
