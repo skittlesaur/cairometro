531fd9c051614876af718c2fb3b0c1daf47eac21
baraa <hey.baraa@gmail.com>
2023-06-09 18:44:04 +0300

    login

--- apps/home/components/authentication/login/login-screen-animation.tsx
+++ apps/home/components/authentication/login/login-screen-animation.tsx
@@ -3,10 +3,8 @@ import PerspectiveGrid from './perspective-grid'
 const LoginScreenAnimation = () => {
 
   return (
-    <div className="h-screen flex flex-col grow gap-10 items-center justify-center ltr:border-l rtl:border-r relative overflow-hidden">
+    <div className="h-screen hidden lg:flex flex-col grow gap-10 items-center justify-center ltr:border-l rtl:border-r relative overflow-hidden">
       <PerspectiveGrid />
-
-     
     </div>
   )
 }
