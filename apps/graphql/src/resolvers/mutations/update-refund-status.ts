1ebdc611b7728c34d2c42cc57f83fac359af41f8
ElBarBary01 <omar.m.elbarbary@gmail.com>
2023-06-10 06:29:40 +0300

    refund

--- apps/graphql/src/resolvers/mutations/update-refund-status.ts
+++ apps/graphql/src/resolvers/mutations/update-refund-status.ts
@@ -1,12 +1,17 @@
+import { TicketType } from '@prisma/client'
 import { arg, FieldResolver } from 'nexus'
+import Stripe from 'stripe'
 
 import adminPermission from '../../permissions/admin'
 
 const updateRefundStatus: FieldResolver< 'Mutation', 'updateRefundStatus' > = async(_, args, ctx)=>{
   const { prisma } = ctx
 
-  // adminPermission(ctx)
-  console.log(args)
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
+    apiVersion: '2022-11-15',
+  })
+  
+  adminPermission(ctx)
   await prisma.refund.update({
     where: {
       id: args.refundRequestId,
@@ -15,6 +20,25 @@ const updateRefundStatus: FieldResolver< 'Mutation', 'updateRefundStatus' > = as
       status: args.status.refundStatus,
     },
   })
+
+  const request = await prisma.refund.findUnique({
+    where: {
+      id: args.refundRequestId,
+    },
+  })
+  if (args.status.refundStatus === 'ACCEPTED' && request.ticketType === TicketType.TICKET){
+
+    const purchase = await prisma.userTickets.findUnique({
+      where: {
+        id: request.referenceId,
+      },
+    })
+
+    await stripe.refunds.create({
+      payment_intent: purchase.paymentId,
+    })
+
+  }
 
   return true
 }
