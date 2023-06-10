0a4de2105128a6c82bc313b15049bf9f73cd2ca0
baraa <hey.baraa@gmail.com>
2023-06-11 00:44:15 +0300

    tickets mobile

--- apps/home/components/ticket.tsx
+++ apps/home/components/ticket.tsx
@@ -25,14 +25,76 @@ const Ticket = (props: TicketProps) => {
 }
 
 const MobileTicket = ({
-  href,
+  departure, arrival, departureTime, arrivalTime, href, price, stations,
 }: TicketProps) => {
+  const { t, i18n } = useTranslation('common')
+
   return (
     <Link
       href={href}
-      className="transition-all duration-150 flex items-center bg-white border border-gray-300 rounded-lg shadow hover:shadow-xl w-full py-6 px-10 justify-between group"
+      className="transition-all duration-150 flex flex-col gap-4 bg-white border border-gray-300 rounded-lg shadow hover:shadow-xl w-full py-6 px-10 justify-between group"
     >
-      @todo: implement mobile ticket (wait for design)
+      <div className="flex flex-col gap-2">
+        <div className="relative w-full flex items-center justify-between">
+          <p className="text-lg font-medium">
+            {departure}
+          </p>
+          <p className="text-sm text-center font-normal text-neutral-400">
+            {departureTime.toLocaleTimeString(
+              i18n.language === 'ar' ? 'ar-EG' : 'en-US',
+              {
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: true,
+              },
+            )}
+          </p>
+          <div className="absolute z-[1] top-1/2 -translate-y-1/2 right-full -translate-x-3 rounded-full bg-white border border-gray-300 min-w-[0.6em] max-w-[0.6em] min-h-[0.6em] max-h-[0.6em]" />
+        </div>
+        <div className="relative">
+          <div className="absolute translate-x-[-1.11em] w-0.5 -top-7 -bottom-7 border-l-2 border-dashed border-gray-200" />
+        </div>
+        <div className="relative w-full flex items-center justify-between">
+          <p className="text-lg font-medium">
+            {arrival}
+          </p>
+          <p className="text-sm text-center font-normal text-neutral-400">
+            {arrivalTime.toLocaleTimeString(
+              i18n.language === 'ar' ? 'ar-EG' : 'en-US',
+              {
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: true,
+              },
+            )}
+          </p>
+          <div className="absolute top-1/2 -translate-y-1/2 right-full -translate-x-3 rounded-full bg-gray-300 border border-gray-300 min-w-[0.6em] max-w-[0.6em] min-h-[0.6em] max-h-[0.6em]" />
+        </div>
+      </div>
+      <div className="flex items-end justify-between">
+        <p className="text-sm font-normal text-neutral-400">
+          {stations} {t('ticket.stations')}
+        </p>
+        <span className="self-end relative text-xl font-semibold text-primary ltr:text-right rtl:text-left">
+          {price === 0 ? (
+            <>
+              {t('ticket.free')}
+              <p className="text-sm font-normal text-neutral-500">
+                {t('ticket.withSubscription')}
+              </p>
+            </>
+          ) : (
+            <>
+              {price.toLocaleString([i18n.language === 'ar' ? 'ar-EG' : 'en-US'], {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}
+              {' '}
+              {t('egpShort')}
+            </>
+          )}
+        </span>
+      </div>
     </Link>
   )
 }
